7af1663050fe5e58d3d24cdafb9813e102755390
rob <robert151196@gmail.com>
2026-03-31 07:28:37 -0300

    feat: simplify has_voted logic in FeatureSerializer and update queryset in FeatureListCreateView

--- backend/features/serializers.py
+++ backend/features/serializers.py
@@ -4,7 +4,7 @@ from .models import Feature
 
 class FeatureSerializer(serializers.ModelSerializer):
     author_username = serializers.CharField(source='author.username', read_only=True)
-    has_voted = serializers.SerializerMethodField()
+    has_voted = serializers.BooleanField(read_only=True)
     is_author = serializers.SerializerMethodField()
 
     class Meta:
@@ -21,13 +21,6 @@ class FeatureSerializer(serializers.ModelSerializer):
             'updated_at',
         ]
         read_only_fields = ['vote_count', 'created_at', 'updated_at', 'author_username']
-
-    def get_has_voted(self, obj):
-        request = self.context.get('request')
-        if not request or not request.user.is_authenticated:
-            return False
-        # votes are prefetch_related'd in the view to avoid N+1
-        return any(v.user_id == request.user.pk for v in obj.votes.all())
 
     def get_is_author(self, obj):
         request = self.context.get('request')
--- backend/features/views.py
+++ backend/features/views.py
@@ -1,12 +1,19 @@
+from django.db.models import Exists, OuterRef
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from votes.models import Vote
 from .models import Feature
 from .serializers import FeatureSerializer
 
 
 class FeatureListCreateView(generics.ListCreateAPIView):
-    queryset = Feature.objects.select_related('author').prefetch_related('votes')
     serializer_class = FeatureSerializer
+
+    def get_queryset(self):
+        return Feature.objects.select_related('author').annotate(
+            has_voted=Exists(
+                Vote.objects.filter(feature=OuterRef('pk'), user=self.request.user)
+            )
+        )
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
